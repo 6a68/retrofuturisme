39279228512ef3d601f6be5f9a95aa771b20b283
Jared Hirsch <ohai@6a68.net>
2014-07-24 00:55:21 -0700

    stopping for the night. main page looks good, child pages are funky though.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var routeMap = {
 
 // XXX this too?
 var port = 7070;
-var host = '127.0.0.1';
+var host = 'localhost';
 
 function onError(e, sock) {
   if (!e) { return; }
@@ -28,30 +28,42 @@ function onError(e, sock) {
 net.createServer(function (socket) {
   socket.setEncoding('ascii');
   socket.on('data', function (data) {
-    // trim those trailing linebreaks to make routing simpler
     console.log("the raw data in the socket is " + data);
+    // trim those trailing linebreaks to make routing simpler
     var endpoint = data.trim();
+    // oh, also strip out any leading slash
+    if (endpoint.indexOf('/') === 0) { endpoint = endpoint.substr(1); }
+
+    // finally, actually route
     if (!endpoint) {
       // the default empty route
+      var page = '';
       console.log('serving default endpoint');
-      var page = 'i\tfake\t(NULL)\t0\r\n' +
-        'i__ R E T R O F U T U R I S M E __\tfake\t(NULL)\t0\r\n' +
-        'iHere we are in gopherspace. Let\'s talk about stuff.\tfake\t(NULL)\t0\r\n';
-      // add the links. just one level deep for now.
-      Object.keys(routeMap).forEach(function (endpoint) {
-        /*
-        page += ':link 0 ' +
-                host + ':' + port + '/' + endpoint + ' ' +
-                routeMap[endpoint]['title'] +
-                '\n';
-        */
-        page += ['0'+routeMap[endpoint]['title'], '/'+endpoint, host, port].join('\t');
-        page += '\r\n';
-      });
+      fs.realpath('./pages/default', function(err, resolvedPath) {
+        if (err) { return onError(e, socket); }
+        fs.readFile(resolvedPath, 'ascii', function (err, contents) {
+          if (err) { return onError(e, socket); }
+          page += contents;
+          // add the links. just one level deep for now.
+          Object.keys(routeMap).forEach(function (endpoint) {
+            var linky = ':link 0 ' +
+                    host + ':' + port + '/' + endpoint + ' ' +
+                    routeMap[endpoint]['title'] + '\n';
+            page += linky;
+          });
 
-      console.log(page);
-      socket.write(page);
-      socket.end();
+          var rendered = '';
+          page.split('\n').forEach(function (item) {
+            console.log('about to gopherize ' + item);
+            var gopherized = gopherize(item);
+            console.log('gopherized into ' + gopherized);
+            rendered += gopherize(item);
+          });
+          console.log(rendered);
+          socket.write(rendered);
+          socket.end();
+        });
+      });
     } else if (endpoint in routeMap) {
       console.log('hit ' + endpoint + ' route');
       console.log('routeMap.endpoint is ' + JSON.stringify(routeMap[endpoint]));
@@ -60,7 +72,12 @@ net.createServer(function (socket) {
         if (err) { return onError(e, socket); }
         fs.readFile(resolvedPath, 'ascii', function (err, contents) {
           if (err) { return onError(e, socket); }
-          socket.write(gopherize(contents));
+          // var page = '';
+          //contents.split('\n').forEach(function(line) { page += gopherize(line); });
+          // socket.write(page);
+          var page = gopherize(contents);
+          console.log(page);
+          socket.write(page);
           socket.end();
         });
       });
